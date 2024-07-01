16579b5ee246cc38ded2511124520b34110a3b88
hong <ghdtmdqor96@gmail.com>
2024-07-01 14:22:20 +0900

    Remove src directory

---
+++ vue.config.js
@@ -0,0 +1,16 @@
+const { defineConfig } = require('@vue/cli-service')
+
+module.exports = defineConfig({
+  transpileDependencies: true,
+  chainWebpack: config => {
+    config.module
+      .rule('raw')
+      .test(/\.txt$/)
+      .use('raw-loader')
+      .loader('raw-loader')
+      .options({
+        esModule: false,
+      })
+      .end();
+  }
+});
